3dbc503a52918f20161e8578e651762f57a7a19a
seongminQa <znznfltm@gmail.com>
2024-05-31 20:56:09 +0900

    JwtProvider 주석 추가

--- src/main/java/com/mycompany/webapp/security/JwtProvider.java
+++ src/main/java/com/mycompany/webapp/security/JwtProvider.java
@@ -46,17 +46,21 @@ public class JwtProvider {
    // AccessToken 생성
    // email 도 저장하고 싶다면, 매개변수에 이메일 정보 추가 
    // .claim("email", email)
+   // authority(권한)이 두 개 이상 가지고 있는 사람이 있다면 List로 받는 것이 좋을 것이다.
    public String createAccessToken(String userId, String authority) {
       String token = null;
       try {
+    	 // Jwt 빌더 객체 생성
          JwtBuilder builder = Jwts.builder();
          // header 설정
          // 자동으로 설정
 
          // payload 설정
          builder.subject(userId);
+         // builder.claim("email", email);
          builder.claim("authority", authority);
          builder.expiration(new Date(new Date().getTime() + accessTokenDuration));
+         //builder.expiration(new Date(new Date().getTime() + 3000));
 
          // signature 설정
          builder.signWith(secretKey);
@@ -67,7 +71,8 @@ public class JwtProvider {
       return token;
    }
 
-   // Access Token 유효성 검증
+   // Access Token 유효성 검증 // 토큰이 유효한지 확인
+   // AccessToken이든 RefreshToken이든 유효성 검사는 같다.
    public Jws<Claims> validateToken(String token) {
       Jws<Claims> jws = null;
       try {
@@ -79,13 +84,13 @@ public class JwtProvider {
          JwtParser parser = builder.build();
          // AccessToken으로부터 payload 얻기
          jws = parser.parseSignedClaims(token);
-      } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+      } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) { // 서버가 알고있는 서명이 아닌 다른 쪽(서버)에서 받은 서명일 
          log.info("잘못된 JWT 서명입니다.");
-      } catch (ExpiredJwtException e) {
+      } catch (ExpiredJwtException e) { // 유효기간 확인
          log.info("만료된 JWT 토큰입니다.");
       } catch (UnsupportedJwtException e) {  
          log.info("지원되지 않는 JWT 토큰입니다.");
-      } catch (IllegalArgumentException e) {
+      } catch (IllegalArgumentException e) { // jwt 구조 자체가 맞지 않은 경우
          log.info("JWT 토큰이 잘못되었습니다.");
       }
       return jws;
@@ -107,9 +112,18 @@ public class JwtProvider {
       return autority;
    }
 
+   // email 까지 확인할 경우
+   /*public String getEmail(Jws<Claims> jws) {
+       //Payload 얻기
+        Claims claims = jws.getPayload();
+       //사용자 권한 얻기
+       String email = claims.get("email").toString();
+        return email;
+    }*/
    
+   // 테스트 용도로 만든 main 메소드
    /*public static final void main(String[] args) {
-      JwtProvider jwtProvider = new JwtProvider("com.mycompany.jsonwebtoken.kosacourse");
+      JwtProvider jwtProvider = new JwtProvider("com.mycompany.jsonwebtoken.kosacourse"); // 테스트용
 
       String accessToken = jwtProvider.createAccessToken("user", "ROLE_USER");
       log.info("AccessToken: " + accessToken);
